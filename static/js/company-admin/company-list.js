433c807d6da66df7fb2c1abb4ebedd9c5b4b2736
ach020614 <ach020614@gmail.com>
2026-01-23 11:16:41 +0900

    기업 메인-목록, html, css 수정 완료

---
+++ static/js/company-admin/company-list.js
@@ -0,0 +1,11 @@
+document.getElementById('datepicker').addEventListener('click', function () {
+    this.showPicker();
+});
+
+document.getElementById('start-date').addEventListener('click', function () {
+    this.showPicker();
+});
+
+document.getElementById('end-date').addEventListener('click', function () {
+    this.showPicker();
+});
